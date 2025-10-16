355b64758111a4c53d11bb99170e41fadd75973a
abismohammad <abismohammad.3@gmail.com>
2025-10-16 23:38:45 +0530

    Updated backend Locking

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,15 +1,13 @@
 terraform {
   backend "s3" {
-
-
-    bucket       = "crisbucket-md"
-    key          = "data/dev/terraform.tfstate"
-    region       = "ap-south-1"
-    use_lockfile = true
-
-
+    bucket         = "crisbucket-md"
+    key            = "data/dev/terraform.tfstate"
+    region         = "ap-south-1"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
   }
 }
+
 
 
 provider "aws" {
