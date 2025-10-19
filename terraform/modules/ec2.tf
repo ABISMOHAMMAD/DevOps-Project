b912b907037d76d0e9b2648e1ee4250a47e1c44e
abismohammad <abismohammad.3@gmail.com>
2025-10-19 08:12:09 +0530

    Added Key pair

--- terraform/modules/ec2.tf
+++ terraform/modules/ec2.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "bastion_host" {
   ami                    = var.ami
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.private_subnet[0].id
+  key_name               = aws_key_pair.bastion_key_pair.key_name
   iam_instance_profile   = aws_iam_instance_profile.bastion_ec2_profile.name
   vpc_security_group_ids = [aws_security_group.security_group.id]
 
@@ -37,6 +38,11 @@ resource "aws_instance" "bastion_host" {
 }
 
 
+resource "aws_key_pair" "bastion_key_pair" {
 
+  key_name   = "${local.project}-bastion-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDSbS7aX/hdlX/ebmv1DdssSHM7/6YH+WSF0vGsCG/L22NJJC4Qq5N4WuqCoeos7lWMJF0+RNY3aFXAwKeL0aNGPfW9cpbRmoA1mhXHF5bQLNUYr8Ou1bNHAQ2AFFoH2bUrvSEmmr4dVtRZVaadW7GMFq7UCLvNxBdnLIGP8alX+8R1L3SJ0vieLzL6szdukWLhyZW5JGgdqA8OddBQkV54hd0QfY+zdQ85Ny9T+11RBpyyZbEIfWXpV5lb1xrD0ZYZdXyp8xVTTZ3KYJF4NSYmOL9h4tlk/eKV8ouYHaQfLF/0tAs1SntcmdGlXbpkzLfKWgGx1H3B5ZQB+0GFbsga6cQknJ1qQOxfhwG4QzaVbI2J6M+Wf755dIIkL+ZSLagS1i5/EdEni4fcYwYdMqm8s1gNX8j672HokWPcEqLwMGZYMC9/E1NLfQwTfQL/rg0+7KRoUT9psrW/svF73g36kYm2olCp+nSvPSVej8v/LSS+aV+QQaGWuoZzEnayhfc= mohammadabis@mohammads-MacBook-Air.local"
+
+}
 
 
